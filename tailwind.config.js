84e7133f99d616fd0844a32b9552b7ee4e2d9828
Aashir <tmohamedaashir@gmail.com>
2023-07-05 22:04:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        chatblack: {
+          50: '#353441fc',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
